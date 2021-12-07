b90b18556367eb2e2a947fa43cd63a840f70f441
Eduard Dub <ed901ed@gmail.com>
2021-12-07 20:11:40 +0200

    new files

---
+++ src/js/03-promises.js
@@ -0,0 +1,35 @@
+import { Notify } from "notiflix";
+
+const refs = {
+  form: document.querySelector('.form'),
+}
+
+refs.form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  const form = e.currentTarget;
+  const times = parseInt(form.elements.amount.value);
+  let delay = parseInt(form.elements.delay.value);
+  let step = parseInt(form.elements.step.value); 
+
+  for (let i = 1; i <= times; i += 1) {
+    createPromise(i, delay).then(value => Notify.success(value, { width: '500px' })).catch(error => Notify.failure(error, { width: '500px' }));
+    delay += step;
+  }
+
+  delay = parseInt(form.elements.delay.value);
+})
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {    
+      if (shouldResolve) {
+        resolve (`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject (`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
